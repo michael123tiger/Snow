1d89dae90a3fa5aa70c754b2226e663c730d58e6
michael123tiger <snowbin19@gmail.com>
2025-03-12 14:15:31 +0530

    this is my program

--- variable/Variable.java
+++ variable/Variable.java
@@ -1,1 +1,9 @@
-asdad
+public class Variable{
+    public static void main(String args[]){
+        int a = 3;
+        int b = 6;
+        int c = a + b;
+        System.out.println("total:"+c);
+
+    }
+}
