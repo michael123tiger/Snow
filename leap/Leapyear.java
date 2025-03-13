99a13e2d5bc1c0f796b03d594678bbfcbb0bf059
michael123tiger <snowbin19@gmail.com>
2025-03-13 14:34:14 +0530

    find Leap year

---
+++ leap/Leapyear.java
@@ -0,0 +1,12 @@
+public class Leapyear {
+    public static void main(String[] args) {
+        int year = 2020;
+
+        // Check if the year is a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
