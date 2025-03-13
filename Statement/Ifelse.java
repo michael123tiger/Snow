69733d56a14eb5ff6cf0515fe91ad7d10ccdac44
michael123tiger <snowbin19@gmail.com>
2025-03-13 11:08:32 +0530

    this is Ifelse statement operator

---
+++ Statement/Ifelse.java
@@ -0,0 +1,12 @@
+public  class Ifelse{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("my name is snow");
+
+        }
+        else{
+            System.out.print("my name is snowtiger");
+
+        }
+    }
+}
