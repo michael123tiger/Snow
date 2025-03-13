6d13a35ef5d1a7a77fbe4b80947d0caf0c104438
michael123tiger <snowbin19@gmail.com>
2025-03-13 14:01:31 +0530

    single array

---
+++ array/Array.java
@@ -0,0 +1,15 @@
+public class Array{
+    public static void main(String[] args) {
+        int[] number={1,2,3,4,5};
+        System.out.println("Array element:");
+        for(int i=0;i<number.length;i++)
+        System.out.println("Element at index"+i+":"+number[i]);
+    }
+    System.out.println("\n Array element using enchanced for loop:");
+    for(int number:number);
+    {
+    {
+        System.out.println(number);
+    }
+}
+}
