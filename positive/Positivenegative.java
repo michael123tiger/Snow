0ecbcae8fc8b18f90c8727d4ead959ad6ddea497
michael123tiger <snowbin19@gmail.com>
2025-03-13 11:28:36 +0530

    this is positive negative value

---
+++ positive/Positivenegative.java
@@ -0,0 +1,14 @@
+public  class Positivenegative{
+    public static void main(String args[]){
+        int number=20;
+        if(number<0){
+            System.out.println("it is negative number");
+
+        }
+        else{
+            System.out.print("it is positive number");
+
+        }
+    }
+}
+
