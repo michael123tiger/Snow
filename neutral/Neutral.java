91e831f06eed2afe9ef82f975cea650483ec341a
michael123tiger <snowbin19@gmail.com>
2025-03-13 11:56:25 +0530

    this is neutral

---
+++ neutral/Neutral.java
@@ -0,0 +1,19 @@
+public  class Neutral{
+    public static void main(String args[]){
+        int number=20;
+        if(number>0){
+            System.out.println("it is negative number");
+
+        }
+        else if(number==0){
+            System.out.println("it is neutral");
+        }
+        else{
+            System.out.print("it is positive number");
+
+        }
+    }
+}
+
+
+
