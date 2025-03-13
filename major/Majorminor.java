18381a78f27ef7a488914e5dc0f5c0adfe960323
michael123tiger <snowbin19@gmail.com>
2025-03-13 11:32:47 +0530

    this is major minor value

---
+++ major/Majorminor.java
@@ -0,0 +1,13 @@
+public  class Majorminor{
+    public static void main(String args[]){
+        int age=20;
+        if(age<18){
+            System.out.println("you are minor");
+
+        }
+        else{
+            System.out.print("you are major");
+
+        }
+    }
+}
