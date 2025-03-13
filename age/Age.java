25565fd39f8ce6c5b11088ae76bd5d1faf0ee0aa
michael123tiger <snowbin19@gmail.com>
2025-03-13 15:43:23 +0530

    AGE

---
+++ age/Age.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Age{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = input.nextInt();
+        System.out.println(age);
+    }
+}
