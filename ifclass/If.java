d7ddb786622681f94877e3f3a368b9a2a85fc5eb
michael123tiger <snowbin19@gmail.com>
2025-03-14 14:55:38 +0530

    ifclass

---
+++ ifclass/If.java
@@ -0,0 +1,12 @@
+public  class If{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("my name is snow");
+
+        }
+        if(false){
+            System.out.print("my name is snowtiger");
+
+        }
+    }
+}
