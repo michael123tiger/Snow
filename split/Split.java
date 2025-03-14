2afbf9ffbbbb472a7231fce733855d9d4bd70c22
michael123tiger <snowbin19@gmail.com>
2025-03-14 11:52:05 +0530

    Split

---
+++ split/Split.java
@@ -0,0 +1,7 @@
+public class Split{
+    public static void main(String args[]){
+        String name = "hello,world";
+        String names[] = name.split(",");
+        System.out.println(names[0]);
+    }
+}
