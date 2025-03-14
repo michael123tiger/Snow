e8a50d725cff9181819802681e21b4443c113343
michael123tiger <snowbin19@gmail.com>
2025-03-14 11:36:53 +0530

    newvowel

---
+++ newvowel/New.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class New{
+    public static void main(String args[]){
+        String v[]={"a","e","i","o","u"};
+        Scanner input =new Scanner(System.in);
+        String answer = input.Nextlin();
+        int detect=0;
+        int i = 0;
+        while(i<=4){
+            if(answer.equals(v[i])){
+            detect = 1;
+          }
+            i=i+1;
+        }
+        if(detect==1){
+            System.out.println("vowel");
+        }
+        else{
+            System.out.println("not a vowels");
+        }
+    }
+}
