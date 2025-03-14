dbb59f413a0c3a010c080cd23f508a69566d6bd4
michael123tiger <snowbin19@gmail.com>
2025-03-14 14:04:30 +0530

    hyraricalinheritance

--- newvowel/New.java
+++ newvowel/New.java
@@ -3,7 +3,7 @@ public class New{
     public static void main(String args[]){
         String v[]={"a","e","i","o","u"};
         Scanner input =new Scanner(System.in);
-        String answer = input.Nextlin();
+        String answer = input.nextlin();
         int detect=0;
         int i = 0;
         while(i<=4){
--- vowels/Vowel.java
+++ vowels/Vowel.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Vowel{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner Scanner = new Scanner(System.in);
         
         System.out.print("Enter a single letter: ");
-        char letter = scanner.next().toLowerCase().charAt(0); // Convert to lowercase for simplicity
+        char letter = Scanner.next().toLowerCase().charAt(0); 
 
         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
             System.out.println("The letter '" + letter + "' is a vowel.");
@@ -15,6 +15,6 @@ public class Vowel{
             System.out.println("That's not a valid letter!");
         }
 
-        scanner.close();
+        Scanner.close();
     }
 }
