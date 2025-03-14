a82eb3def31bd58e28866a64e99810bba854fda0
michael123tiger <snowbin19@gmail.com>
2025-03-14 12:06:09 +0530

    lower case

---
+++ upper lower case/Lowercases.java
@@ -0,0 +1,8 @@
+public class Lowercases{
+    public static void main(String args[]){
+        String name = "hello,world";
+        
+        System.out.println(name.tolowerCase());
+       
+    }
+}
